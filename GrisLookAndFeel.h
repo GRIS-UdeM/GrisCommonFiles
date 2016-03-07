d15893a5ba470e383ef3acb94ddec2a7e92e4170
Vincent Berthiaume <vberthiaume@gmail.com>
2016-03-07 12:19:11 -0500

    drawing tab buttons correctly

--- GrisLookAndFeel.h
+++ GrisLookAndFeel.h
@@ -255,7 +255,6 @@ public:
         
         g.setColour (button.findColour (ToggleButton::textColourId));
         g.setFont(m_Font);
-//        g.setFont (fontSize);
         
         if (! button.isEnabled())
             g.setOpacity (0.5f);
@@ -268,25 +267,76 @@ public:
                           Justification::centredLeft, 10);
     }
     
-    void drawTabButtonText (TabBarButton& button, Graphics& g, bool isMouseOver, bool isMouseDown) override {
+    void drawTabButton (TabBarButton& button, Graphics& g, bool isMouseOver, bool isMouseDown) override{
+        const Rectangle<int> activeArea (button.getActiveArea());
+        
+        
+        const TabbedButtonBar::Orientation o = button.getTabbedButtonBar().getOrientation();
+        
+        const Colour bkg (button.getTabBackgroundColour());
+        
+        if (button.getToggleState())
+        {
+            g.setColour (bkg);
+        }
+        else
+        {
+            Point<int> p1, p2;
+            
+            switch (o)
+            {
+                case TabbedButtonBar::TabsAtBottom:   p1 = activeArea.getBottomLeft(); p2 = activeArea.getTopLeft();    break;
+                case TabbedButtonBar::TabsAtTop:      p1 = activeArea.getTopLeft();    p2 = activeArea.getBottomLeft(); break;
+                case TabbedButtonBar::TabsAtRight:    p1 = activeArea.getTopRight();   p2 = activeArea.getTopLeft();    break;
+                case TabbedButtonBar::TabsAtLeft:     p1 = activeArea.getTopLeft();    p2 = activeArea.getTopRight();   break;
+                default:                              jassertfalse; break;
+            }
+            
+            g.setGradientFill (ColourGradient (bkg.brighter (0.2f), (float) p1.x, (float) p1.y,
+                                               bkg.darker (0.1f),   (float) p2.x, (float) p2.y, false));
+        }
+        
+        g.fillRect (activeArea);
+        
+        g.setColour (button.findColour (TabbedButtonBar::tabOutlineColourId));
+        
+        Rectangle<int> r (activeArea);
+        
+        if (o != TabbedButtonBar::TabsAtBottom)   g.fillRect (r.removeFromTop (1));
+        if (o != TabbedButtonBar::TabsAtTop)      g.fillRect (r.removeFromBottom (1));
+        if (o != TabbedButtonBar::TabsAtRight)    g.fillRect (r.removeFromLeft (1));
+        if (o != TabbedButtonBar::TabsAtLeft)     g.fillRect (r.removeFromRight (1));
+        
+        const float alpha = button.isEnabled() ? ((isMouseOver || isMouseDown) ? 1.0f : 0.8f) : 0.3f;
+        
+        Colour col (bkg.contrasting().withMultipliedAlpha (alpha));
+        
+        if (TabbedButtonBar* bar = button.findParentComponentOfClass<TabbedButtonBar>())
+        {
+            TabbedButtonBar::ColourIds colID = button.isFrontTab() ? TabbedButtonBar::frontTextColourId
+            : TabbedButtonBar::tabTextColourId;
+            
+            if (bar->isColourSpecified (colID))
+                col = bar->findColour (colID);
+            else if (isColourSpecified (colID))
+                col = findColour (colID);
+        }
+        
         const Rectangle<float> area (button.getTextArea().toFloat());
         
         float length = area.getWidth();
         float depth  = area.getHeight();
         
-        if (button.getTabbedButtonBar().isVertical()){
+        if (button.getTabbedButtonBar().isVertical())
             std::swap (length, depth);
-        }
         
-        //        Font font (depth * 0.6f);
-        Font font(m_Font);
-        font.setHeight(depth * 0.6f);
-        cout << "SpatGris drawTabButtonText\n";
-        font.setUnderline (button.hasKeyboardFocus (false));
+        TextLayout textLayout;
+        createTabTextLayout (button, length, depth, col, textLayout);
         
         AffineTransform t;
         
-        switch (button.getTabbedButtonBar().getOrientation()) {
+        switch (o)
+        {
             case TabbedButtonBar::TabsAtLeft:   t = t.rotated (float_Pi * -0.5f).translated (area.getX(), area.getBottom()); break;
             case TabbedButtonBar::TabsAtRight:  t = t.rotated (float_Pi *  0.5f).translated (area.getRight(), area.getY()); break;
             case TabbedButtonBar::TabsAtTop:
@@ -294,22 +344,71 @@ public:
             default:                            jassertfalse; break;
         }
         
-        Colour col;
-        
-        if (button.isFrontTab() && (button.isColourSpecified (TabbedButtonBar::frontTextColourId) || isColourSpecified (TabbedButtonBar::frontTextColourId))){
-            col = findColour (TabbedButtonBar::frontTextColourId);
-        } else if (button.isColourSpecified (TabbedButtonBar::tabTextColourId) || isColourSpecified (TabbedButtonBar::tabTextColourId)){
-            col = findColour (TabbedButtonBar::tabTextColourId);
-        } else {
-            col = button.getTabBackgroundColour().contrasting();
-        }
-        const float alpha = button.isEnabled() ? ((isMouseOver || isMouseDown) ? 1.0f : 0.8f) : 0.3f;
-        g.setColour (col.withMultipliedAlpha (alpha));
-        g.setFont (font);
         g.addTransform (t);
-        g.drawFittedText (button.getButtonText().trim(), 0, 0, (int) length, (int) depth, Justification::centred, jmax (1, ((int) depth) / 12));
+        textLayout.draw (g, Rectangle<float> (length, depth));
     }
+    
+    void createTabTextLayout (const TabBarButton& button, float length, float depth, Colour colour, TextLayout& textLayout)
+    {
+        Font font (m_Font);
+        font.setHeight(depth * 0.35f);
+        font.setUnderline (button.hasKeyboardFocus (false));
         
+        AttributedString s;
+        s.setJustification (Justification::centred);
+        s.append (button.getButtonText().trim(), font, colour);
+        
+        textLayout.createLayout (s, length);
+    }
+    
+//    void drawTabButtonText (TabBarButton& button, Graphics& g, bool isMouseOver, bool isMouseDown) override
+//    {
+//        const Rectangle<float> area (button.getTextArea().toFloat());
+//        
+//        float length = area.getWidth();
+//        float depth  = area.getHeight();
+//        
+//        if (button.getTabbedButtonBar().isVertical())
+//            std::swap (length, depth);
+//        
+//        Font font (m_Font);
+//        font.setHeight(depth * 0.35f);
+//        font.setUnderline (button.hasKeyboardFocus (false));
+//        
+//        AffineTransform t;
+//        
+//        switch (button.getTabbedButtonBar().getOrientation())
+//        {
+//            case TabbedButtonBar::TabsAtLeft:   t = t.rotated (float_Pi * -0.5f).translated (area.getX(), area.getBottom()); break;
+//            case TabbedButtonBar::TabsAtRight:  t = t.rotated (float_Pi *  0.5f).translated (area.getRight(), area.getY()); break;
+//            case TabbedButtonBar::TabsAtTop:
+//            case TabbedButtonBar::TabsAtBottom: t = t.translated (area.getX(), area.getY()); break;
+//            default:                            jassertfalse; break;
+//        }
+//        
+//        Colour col;
+//        
+//        if (button.isFrontTab() && (button.isColourSpecified (TabbedButtonBar::frontTextColourId)
+//                                    || isColourSpecified (TabbedButtonBar::frontTextColourId)))
+//            col = findColour (TabbedButtonBar::frontTextColourId);
+//        else if (button.isColourSpecified (TabbedButtonBar::tabTextColourId)
+//                 || isColourSpecified (TabbedButtonBar::tabTextColourId))
+//            col = findColour (TabbedButtonBar::tabTextColourId);
+//        else
+//            col = button.getTabBackgroundColour().contrasting();
+//        
+//        const float alpha = button.isEnabled() ? ((isMouseOver || isMouseDown) ? 1.0f : 0.8f) : 0.3f;
+//        
+//        g.setColour (col.withMultipliedAlpha (alpha));
+//        g.setFont (font);
+//        g.addTransform (t);
+//        
+//        g.drawFittedText (button.getButtonText().trim(),
+//                          0, 0, (int) length, (int) depth,
+//                          Justification::centred,
+//                          jmax (1, ((int) depth) / 12));
+//    }
+    
 //        int getTabButtonOverlap (int /*tabDepth*/)            override { return -1; }
 //        int getTabButtonSpaceAroundImage()                    override { return 0; }
 
